f1545e0e7adfc40f63815a5c4040b31e6031af08
Frank Monteon <frank.monteon@gmail.com>
2023-05-05 11:33:12 -0600

    finished the contact component

--- vite-project/src/components/pages/Resume.jsx
+++ vite-project/src/components/pages/Resume.jsx
@@ -1,14 +1,7 @@
+import React, { useState } from 'react';
+
 const styles = {
-  h1: {
-    margin: 20,
-    background: '#3f51b5',
-    minHeight: 50,
-    lineHeight: 3.5,
-    fontSize: '1.2rem',
-    color: 'white',
-    padding: '0 20px',
-    textAlign: 'center',
-  },
+
   content: {
     display: 'flex',
     flexDirection: 'column',
@@ -27,17 +20,16 @@ const styles = {
 export default function Resume() {
   return (
     <div>
-      <h1 style={styles.h1}>Resume</h1>
       <div style={styles.content}>
         <div style={styles.paragraph}>
           <p>
-            These are the technologies that I have learned during the bootcamp:
+            Here are the technologies I have used:
           </p>
           <div style={styles.paragraphWithBackground}>
             <h5>Frontend:</h5>
             <ul>
-              <li>HTML5</li>
-              <li>CSS3</li>
+              <li>HTML</li>
+              <li>CSS</li>
               <li>JavaScript</li>
               <li>jQuery</li>
               <li>Bootstrap</li>
