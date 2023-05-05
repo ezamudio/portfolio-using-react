46955609ee3eb7aa26a835d025f73f862802eb38
Frank Monteon <frank.monteon@gmail.com>
2023-05-05 12:08:17 -0600

    header component completed

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
 function Header({ currentPage }) {
-  let headerTitle = '';
-  if (currentPage === 'About') {
-    headerTitle = 'About Me';
-  } else if (currentPage === 'Resume') {
-    headerTitle = 'Resume';
-  } else if (currentPage === 'Blog') {
-    headerTitle = 'Blog';
-  } else if (currentPage === 'Contact') {
-    headerTitle = 'Contact';
+  let headerText;
+  switch (currentPage) {
+    case 'About':
+      headerText = 'About Me';
+      break;
+    case 'Resume':
+      headerText = 'Resume';
+      break;
+    case 'Blog':
+      headerText = 'Blog';
+      break;
+    case 'Contact':
+      headerText = 'Contact';
+      break;
+    default:
+      headerText = 'Home';
   }
 
   return (
     <header className="header">
-      <h1>{headerTitle}</h1>
+      <h1>{headerText}</h1>
     </header>
   );
 }
--- vite-project/src/components/NavTabs.jsx
+++ vite-project/src/components/NavTabs.jsx
@@ -1,46 +1,51 @@
 import React from 'react';
+import '../components/pages/styles/styles.css';
+import Header from './Header';
 
-function NavTabs({ currentPage, handlePageChange }) {
+function Navbar({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About Me
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#Resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <nav>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Resume"
+            onClick={() => handlePageChange('Resume')}
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#blog"
+            onClick={() => handlePageChange('Blog')}
+            className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+          >
+            Blog
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+      <Header currentPage={currentPage} />
+    </nav>
   );
 }
 
-export default NavTabs;
+export default Navbar;
