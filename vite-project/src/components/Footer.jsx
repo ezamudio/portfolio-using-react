a2899392385323d75eb840cdba5241d433747b6c
Frank Monteon <frank.monteon@gmail.com>
2023-05-05 22:28:46 -0600

    added logos to the footer

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import githublogo from "./pages/images/githublogo.jpeg";
+import linkedinlogo from "./pages/images/linkedinlogo.jpeg";
 
 const styles = {
   footer: {
@@ -8,13 +10,24 @@ const styles = {
     borderTop: '1px solid #ddd',
     marginTop: '200px',
   },
+  logo: {
+    width: '40px',
+    height: '40px',
+    margin: '0 10px',
+  },
 };
-
 
 const Footer = () => (
   <div style={styles.footer}>
     <p> Francisco Monteon</p>
+    <a href="https://github.com/MonteonFrank?tab=repositories">
+      <img src={githublogo} alt="Github" style={styles.logo} />
+    </a>
+    <a href="https://www.linkedin.com/in/francisco-javier-monteon-avila-25898a99/">
+      <img src={linkedinlogo} alt="LinkedIn" style={styles.logo} />
+    </a>
   </div>
 );
 
 export default Footer;
+
